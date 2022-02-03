9dd105ac813b6a10dea8ff71716734361a441e91
serayaGirya <sergey_girko@mail.ru>
2022-02-03 16:54:38 +0300

    memory usage of vector

---
+++ cmake-build-debug/CMakeFiles/A_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A_1.exe"
+  "A_1.exe.manifest"
+  "A_1.pdb"
+  "CMakeFiles/A_1.dir/main.c.obj"
+  "libA_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
